fc3c49eb87375e52730d5d3b692ffb3d70ef2de4
?ngel ?lvarez <angel@nan-tic.com>
2014-09-17 15:15:14 +0200

    add funcionality of project_allocation_repor and project_allocation_edit_inline, this module are deprecated

--- work.py
+++ work.py
@@ -78,6 +78,11 @@ class Work:
             depends=['type']),
         'get_project_dates', setter='set_expected_end_date_project')
 
+    assigned_employee = fields.Function(fields.Many2One('company.employee',
+            'Assigned'), 'get_assigned_employee',
+        setter='set_assigned_employee', searcher='search_assigned_employee')
+
+
     @classmethod
     def __setup__(cls):
         super(Work, cls).__setup__()
@@ -169,6 +174,30 @@ class Work:
         cls.write(childs, {
                 'expected_end_date': value,
                 })
+
+    def get_assigned_employee(self, name):
+        if self.allocations:
+            return self.allocations[0].employee.id
+
+    @classmethod
+    def set_assigned_employee(cls, works, name, value):
+        Allocation = Pool().get('project.allocation')
+        Allocation.delete([allocation for work in works
+                for allocation in work.allocations])
+        if value:
+            to_create = []
+            for work in works:
+                to_create.append({
+                        'employee': value,
+                        'work': work.id,
+                        })
+            Allocation.create(to_create)
+
+    @classmethod
+    def search_assigned_employee(cls, name, clause):
+        if clause[2] is None:
+            return [('allocations',) + tuple(clause[1:])]
+        return [('allocations.employee',) + tuple(clause[1:])]
 
 
 class PredecessorSuccessor(ModelSQL):
